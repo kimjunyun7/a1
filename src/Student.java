ae5b23f9813bb428557d44f740890f311d6f1d08
kimjunyun7 <kimjunyun7@gmail.com>
2021-04-08 17:26:33 +0700

    edit Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,7 +1,37 @@
 import java.time.LocalDate;
 
-public class Student {
-    String id;
-    String name;
-    LocalDate birthdate;
+class Student {
+    private String id;
+    private String name;
+    private LocalDate birthdate;
+
+    Student(String id, String name, LocalDate birthdate) {
+        this.id = id;
+        this.name = name;
+        this.birthdate = birthdate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
 }
