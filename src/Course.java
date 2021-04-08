e80a55d733151bb79a812e6855b0340450889f5e
kimjunyun7 <kimjunyun7@gmail.com>
2021-04-08 17:22:11 +0700

    add Course.class

---
+++ src/Course.java
@@ -0,0 +1,35 @@
+public class Course {
+    private String id;
+    private String name;
+    private int numOfCredit;
+
+    Course(String id, String name, int numOfCredit) {
+        this.id = id;
+        this.name = name;
+        this.numOfCredit = numOfCredit;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumOfCredit() {
+        return numOfCredit;
+    }
+
+    public void setNumOfCredit(int numOfCredit) {
+        this.numOfCredit = numOfCredit;
+    }
+}
